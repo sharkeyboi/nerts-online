a71e53ad74ed1fecf6d1036e0ae42f7cf48f18d8
Eric Sharkey <eric@callsimulator.com>
2023-11-26 20:51:46 -0500

    Updated url

--- plugins/socket.io.ts
+++ plugins/socket.io.ts
@@ -2,7 +2,7 @@ import { io, Socket } from 'socket.io-client'
 import type { ClientToServerEvents, ServerToClientEvents } from '~/src/types/socketMessages'
 
 export default defineNuxtPlugin(() => {
-    const socket: Socket<ServerToClientEvents, ClientToServerEvents> = io('localhost:3000', {
+    const socket: Socket<ServerToClientEvents, ClientToServerEvents> = io('https://nerts-online-lwxkhv64cq-uc.a.run.app', {
         autoConnect: false,
         multiplex: false,
     })
